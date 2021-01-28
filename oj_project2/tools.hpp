938ac0f97331458728819f60b60714c302e5b4f8
lianlian51 <3282125330@qq.com>
2021-01-28 16:56:24 +0800

    tools

--- oj_project2/tools.hpp
+++ oj_project2/tools.hpp
@@ -6,6 +6,7 @@
 #include <boost/algorithm/string.hpp>
 #include <vector>
 #include <unordered_map>
+#include <fstream>
 
 class FileUtil
 {
@@ -158,4 +159,48 @@ class TimeUtil
       gettimeofday(&tv, NULL);
       return tv.tv_sec + tv.tv_usec / 1000;
     }
+
+    static void GetTimeStamp(std::string* TimeStamp)
+    {
+      time_t st;
+      time(&st);
+
+      struct tm* t = localtime(&st);
+      
+      char buf[30] = {0};
+      snprintf(buf, sizeof(buf) - 1, "%04d-%02d-%02d %02d:%02d:%02d", t->tm_year + 1900, t->tm_mon + 1, t->tm_mday, t->tm_hour, t->tm_min, t->tm_sec);
+      TimeStamp->assign(buf, strlen(buf));
+    }
 };
+
+enum LogLevel
+{
+  INFO = 0,
+  WARNING,
+  ERROR,
+  FATAL,
+  DEUBG 
+};
+
+
+const char* Level[] = 
+{
+  "INFO",
+  "WARNING",
+  "ERROR",
+  "FATAL",
+  "DEUBG"
+};
+
+inline std::ostream& Log(LogLevel lev, const char* file, int line, const std::string& logmsg)
+{
+  std::string level_info = Level[lev];
+  std::string TimeStamp;
+  TimeUtil::GetTimeStamp(&TimeStamp);
+  
+  std::cout << "[" << TimeStamp << " " << level_info << " " << file << ":" << line << "]" << " " << logmsg;
+  return std::cout;
+}
+
+#define LOG(lev, msg) Log(lev, __FILE__, __LINE__, msg)
+
