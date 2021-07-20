b7882e126ff0c1efe7d933492fb2b38dac3be78b
lianlian51 <3282125330@qq.com>
2021-07-20 21:26:49 +0800

    HTCompress.h

---
+++ zip/zip/LZ77/HTCompress.h
@@ -0,0 +1,51 @@
+#pragma once
+#include "HuffmanTree.hpp"
+
+#include<string>
+#include<vector>
+
+typedef unsigned long long ulg;
+typedef unsigned char uch;
+
+struct CharInfo
+{
+	uch _ch;
+	ulg _appearCount; // 字符出现的次数
+	std::string _strCode; // 字符对应的编码
+
+	CharInfo(ulg appCount = 0)
+		: _appearCount(appCount)
+	{}
+
+	CharInfo operator+(const CharInfo& c)
+	{
+		return CharInfo(_appearCount + c._appearCount);
+	}
+
+	bool operator>(const CharInfo& c) const
+	{
+		return _appearCount > c._appearCount;
+	}
+
+	bool operator==(const CharInfo& c)
+	{
+		return _appearCount == c._appearCount;
+	}
+};
+
+class HTCompress
+{
+public:
+
+	HTCompress();
+	void CompressFile(const std::string& filePath);
+	void UNCompressFile(const std::string& filePath);
+
+	void WriteHeadInfo(FILE* fOut, const std::string& filePath);
+
+private:
+	void GeneteCode(HuffmanTreeNode<CharInfo>* root);
+	void GetLine(FILE* fIn, std::string &s);
+private:
+	std::vector<CharInfo> _charInfo;
+};
